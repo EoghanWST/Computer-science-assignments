199439b789c2aa6b343edbd97bf653b216dbddf5
EoghanWST <144443626+EoghanWST@users.noreply.github.com>
2023-09-08 11:42:19 +0100

    Add files via upload

---
+++ comp sci 2.py
@@ -0,0 +1,32 @@
+print(str)
+
+ 
+
+h = ("i live in athlone")
+numa = (50)
+numz = (65)
+
+ 
+
+print( numa + numz )
+print( numa * numz )
+
+ 
+
+print ("HELLO I AM" ,23, "yrs old", h)
+#single line comment
+
+ 
+
+#""" multi
+#line
+#comment
+#"""
+
+ 
+
+ 
+
+age =  input("Enter your age    :    ")
+age2 = input (" Enter another age ")
+print("cool!!")#
---
+++ comp sci challange 1.py
@@ -0,0 +1,32 @@
+"""#challange part 1
+print ("what is your name")
+#name = input()
+#print ("Hello",name)
+
+#challange part 2
+#print ("what is your first name")
+#name = input()
+#print ("what is your surnname")
+#surname = input()
+#print ("Hello",name,surname)
+
+#challange part 3
+#print( "What do you call a bear with no teeth\nA gummy bear!")
+
+callange part 4
+num1 = input("enter the first number ")
+num2 = input("enter the second number ")
+num1 = int(num1)
+num2 = int(num2)
+print (num1+num2)
+
+#challange part 5
+#num1 = input("enter the first number ")
+#num2 = input("enter the second number ")
+#num3 = input("enter the number to multiply the sum of the other two by it ")
+#num1 = int(num1)
+#num2 = int(num2)
+#num3 = int(num3)
+#print ((num1+num2)*num3)"""
+
+#challange part 6
