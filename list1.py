d36f0583cd00912b827ae9f68898effc0ad1f579
EoghanWST <144443626+EoghanWST@users.noreply.github.com>
2023-09-20 12:43:13 +0100

    Add files via upload

---
+++ list1.py
@@ -0,0 +1,15 @@
+list1 = ["big","small","large","tall","tall","tall","tall","tall"]
+print (list1)
+
+x = list1[3]
+
+print (x)
+print(list1.count(x))
+list1.insert(2,"tiny")
+print(list1)
+list1.remove("big")
+print(list1)
+list1.reverse()
+print(list1)
+list1.sort()
+print(list1)
