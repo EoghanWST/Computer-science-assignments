51f00edd83cf2b0e96231ede9b20301c6492995e
EoghanWST <144443626+EoghanWST@users.noreply.github.com>
2023-09-20 13:12:22 +0100

    Add files via upload

---
+++ list challange.py
@@ -0,0 +1,28 @@
+#1
+"""sports=["Football","rugby"]
+a = (input("whats is you favorite sport? "))
+sports.insert(2,a)
+sports.sort()
+print(sports)"""
+
+#2
+"""subjects = ["english","irish","maths","business","computer science","history"]
+print (subjects)
+x = input("which is your least favorite subject? ")
+subjects.remove(x)
+print(subjects) """
+
+#3
+"""colour = ["red","blue","green","yellow","black","white","purple","orange","pink","gold"]
+A = input("input a number between 0 abd 4,")
+B = input("input a number between 5 abd 9,")
+A1 = int(A)
+B1 = int(B)
+C = colour[A1]
+D = colour[B1]
+colouf =[A1,C,D,B1]
+print(colouf)"""
+
+#4
+3dig = ["111\nA","222\nA","333\nA","444\nA"]
+print(3dig)
