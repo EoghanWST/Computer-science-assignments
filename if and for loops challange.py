02eb1259e582223d6bbb8e7601809c47e6d493f6
EoghanWST <144443626+EoghanWST@users.noreply.github.com>
2023-09-27 13:08:01 +0100

    Add files via upload

---
+++ if and for loops challange.py
@@ -0,0 +1,108 @@
+#12
+
+ 
+
+"""a = int(input("give me a number "))
+
+ 
+
+b = int(input("give me a second number "))
+
+ 
+
+ 
+
+if a > b:
+    print(b,"and",a )
+
+if  a < b:
+    print(a, "and" ,b)"""
+
+"""#13   
+r = int(input("Enter a number that is below 20 "))
+
+ 
+
+if r > 20:
+    print("too HIGH!!")
+
+if r == 20 or r < 20:
+        print("thank you")"""
+        
+#14  
+"""E = int(input("Enter a number between 10 and 20 "))
+
+ 
+
+if E < 10:
+    print("incorrect")
+
+if E == 20 or E < 20:
+        print("correct")"""
+
+#15
+"""b = "red"
+c = "RED"
+wa = (input("whats your favourite colour "))
+if wa == b or wa == c:
+       print(" I like red too")
+else:
+    print("I dont like", wa , " i prefer red")"""
+
+#16
+"""string1 = input("is it raining ")
+string2 = string1.lower()
+print (string2)
+
+if string2 == ("yes"):
+    string3 = input("is it windy ")
+    string4 = string3.lower()
+    if string4 == ("no"):
+        print("take an umbrella!")
+    if string4 == ("yes"):
+       string5 = input("is it to eindy to take an umbrella ")
+       string6 = string5.lower()
+    if string6 == ("yes"):
+             print ("enjoy your day")
+    
+    else:
+        print ("take an ubrella!")
+
+else:
+    print("enjoy your day")"""
+
+"""#17
+age = int(input("what age are you "))
+if age == 18 or age > 19:
+    print("you are old enough to vote")
+if age == 17:
+    print("you can learn to drive")
+if age == 16:
+    print("you can buy a lottery ticket")
+if age < 16:
+    print("you can go trick or treating")"""
+
+#19
+"""A = int(input("ENTER 1 OR 2 Or 3 "))
+if A == 1:
+    print("thank you")
+if A == 2:
+    print("well done")
+if A == 3:
+    print("correct")"""
+        
+#37
+name = input("what is your name")
+
+
+print (name[0])
+print (name[1])
+print (name[2])
+print (name[3])
+print (name[4])
+print (name[5])
+
+
+         
+         
+         
