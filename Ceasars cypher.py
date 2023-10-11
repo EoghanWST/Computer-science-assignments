734c650e0ee48a105890cb7e7bdb7ae98d90dca5
EoghanWST <144443626+EoghanWST@users.noreply.github.com>
2023-10-11 12:52:31 +0100

    Update Ceasars cypher.py

--- Ceasars cypher.py
+++ Ceasars cypher.py
@@ -10,15 +10,30 @@
 k=0
 cyp  = ["A","B","C","D","E","F","H","I","J","K","L","M","N","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 new_string = ""
-abc = input("What is your message")
-key = int(input ("what is your key"))
-for ch in abc:
-    if ch in cyp:
-        index_position = cyp.index(ch)
-        k= index_position + key
-        if k > 25:
-            k = k%26
+C = input("Enter A or B to code or decode your message")
+if C == "A":
+    abc = input("What is your message")
+    key = int(input ("what is your key"))
+    for ch in abc:
+        if ch in cyp:
+            index_position = cyp.index(ch)
+            k= index_position + key
+            if k > 25:
+                k = k%26
         
-        new_string = new_string + cyp[k]
-print(new_string)
+            new_string = new_string + cyp[k]
+    print(new_string)
 
+cyp = ["A","B","C","D","E","F","H","I","J","K","L","M","N","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+k=0
+new_string = ""
+if C == "B":
+    abc = input("What is your message")
+    key = int(input ("what is your key"))
+    for ch in abc:
+        if ch in cyp:
+            index_position = cyp.index(ch)
+            k= index_position - key
+       
+            new_string = new_string + cyp[k]
+    print(new_string)
