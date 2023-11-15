1ccd4b5c38c936c012230bd9ea3ada5d78cd5c36
EoghanWST <144443626+EoghanWST@users.noreply.github.com>
2023-11-15 12:05:46 +0000

    Add files via upload

---
+++ strings 3.py
@@ -0,0 +1,61 @@
+#80
+"""a = input("what is your first name")
+aa = len(a)
+print(aa)
+b = input("what is your surname")
+ba = len(a)
+print(ba)
+c = (a + " " + b)
+print(c)
+ca = len(c)
+print(ca)"""
+
+#81
+"""a = input("what is your favourite subject")
+b = len(a)
+c = 0
+while c != b - 1:
+    print( a[c], end="-")
+    c = c + 1
+print(a[c])"""
+
+#82
+"""a = ("The first few coughs are slow dances and waltz")
+b = len(a)
+print(a)
+c = int(input("starting point"))
+d = int(input("end point"))
+e = d - c
+print(e)
+f = 0
+while f != e:
+    print(a[c + 1])
+    c = c + 1
+    f = f + 1"""
+
+#83 string.isupper()
+"""while True:
+    a = input("type a word in uppercases")
+    b = a.isupper()
+    if b == True:
+        print("good")
+        break
+    if b != True:
+        print("try again")"""
+
+#84 fix this
+"""a = input("Enter your post code")
+b = (a[0])
+c = (a[1])
+b = (b.upper())
+c = (c.upper())
+print(b, end="")
+print(c)"""
+
+
+
+
+
+
+
+
